1eabb25db4244de5463a5eaef84f925dc8f1dfbe
José Miguel <josemlp.montefrio@gmail.com>
2016-09-11 22:01:07 +0200

    Update RunDriver2IndiServer.sh

--- RunDriver2IndiServer.sh
+++ RunDriver2IndiServer.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#http://stackoverflow.com/questions/10751304/java-lang-unsatisfiedlinkerror-no-rxtxserial-in-java-library-path-thrown-while
-
 java  -Djava.library.path=/usr/lib/jni -Djava.library.path=/usr/lib/rxtx  -jar ./I4JServer/dist/I4JServer.jar -add=./I4JArdufocuserDriver/dist/I4JArdufocuserDriver.jar
